08d2837c31ad8d783ce8412c619f7ea045f6ae6d
Chitta Ranjan Ojha <129220280+mana6969@users.noreply.github.com>
2023-07-14 15:30:28 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 2205298041
+let accountEmail = "ojhac150@gmail.com"
+var accountPassword = "manaojha143"
+accountCity = "Jajpur"
+let accountState
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// accountId = 2  //not allowed
+accountEmail = "chittaranjan2022@gift.edu.in"
+accountPassword = "manaojha"
+accountCity = "BBSR"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
